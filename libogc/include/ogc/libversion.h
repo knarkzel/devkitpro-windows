5168554661c4e8b1e0677e066b9e7920a292226d
Knarkzel <knarkzel@gmail.com>
2022-06-14 20:03:23 +0200

    Initial commit

---
+++ libogc/include/ogc/libversion.h
@@ -0,0 +1,13 @@
+#ifndef __LIBVERSION_H__
+#define __LIBVERSION_H__
+
+#define _V_MAJOR_	2
+#define _V_MINOR_	3
+#define _V_PATCH_	1
+
+#define _V_DATE_			__DATE__
+#define _V_TIME_			__TIME__
+
+#define _V_STRING "libOGC Release 2.3.1"
+
+#endif // __LIBVERSION_H__
